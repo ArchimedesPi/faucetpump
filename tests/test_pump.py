de98d92490955d2eb6f3b5db5f1e89066564695a
Liam <liam@cpan.org>
2016-08-26 12:46:17 -0500

    Update pump tests to use new syntax

--- tests/test_pump.py
+++ tests/test_pump.py
@@ -1,7 +1,8 @@
 from faucetpump.pump import Pump
 
 def test_creation():
-	pump = Pump('A Test Pump', run='every 1m', currency='btc')
+	pump = Pump('A Test Pump', run='every 1m', currency='btc', addr_pool=True,
+		addr_cooldown='3d')
 
 	@pump.run
 	def foo():
