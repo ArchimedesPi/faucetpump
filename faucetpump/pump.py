948100d3a21b250e9ba8ab1809eace6e5c54a247
Liam <liam@cpan.org>
2016-08-26 12:45:40 -0500

    Tweak pump api slightly

--- faucetpump/pump.py
+++ faucetpump/pump.py
@@ -4,10 +4,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Pump(object):
-	def __init__(self, fqname, run='every 1min', currency=None):
+	def __init__(self, fqname, run='every 1min', currency=None, addr_pool=None, addr_cooldown=None):
 		self.fqname = fqname
 		self.schedule = run
-		self.payout = dict(currency=currency)
+		self.payout = dict(currency=currency, addr_pool=addr_pool or False, addr_cooldown=addr_cooldown)
 
 	def run(self, f):
 		self.run_function = f
